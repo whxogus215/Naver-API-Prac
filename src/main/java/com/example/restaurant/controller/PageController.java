2ad2e4eb8b6b3987d6898cdf31be5e79ecdfbbdb
whxogus215 <whxogus215@naver.com>
2023-07-06 08:24:09 +0900

    Ch 10 완강

---
+++ src/main/java/com/example/restaurant/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.example.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pages")
+public class PageController {
+
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
